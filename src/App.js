a2e4474bcdf8c90daa40ffbd5d1ca597f86d8f1e
TonyLiu0226 <TL0226yn@gmail.com>
2022-11-06 09:33:57 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    var responseClone; // 1
+  fetch("http://localhost:3001/api", {mode:'cors'})
+  .then(function (response) {
+    responseClone = response.clone(); // 2
+    return response.json();
+    })
+    .then(function (data) {
+      setData(data);
+    }, function (rejectionReason) { // 3
+    console.log('Error parsing JSON from response:', rejectionReason, responseClone); // 4
+    responseClone.text() // 5
+    .then(function (bodyText) {
+        console.log('Received the following instead of valid JSON:', bodyText); // 6
+    });
+})});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(typeof data.users === 'undefined') ?
+      (<p>Loading...</p>): (
+        data.users.map((user, i) => (
+          <p key={i}>({user})</p>
+        ))
+      ) }
     </div>
   );
 }
